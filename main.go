1c1b4635af8a7e6b4f98e362bbe4883a4eaa4a61
cclshub <97969453+cclshub@users.noreply.github.com>
2023-11-02 04:27:17 +0800

    incomplete initNode: most fields still empty

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -11,23 +12,41 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-func main () {
+func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
 	}
+
 	runGrpcServer(config)
 }
 
-func runGrpcServer (config util.Config) {
+func initNode(config util.Config, s *grpc.Server) {
+	chordID := config.ChordId
+	var num int64
+	_, err := fmt.Sscanf(chordID, "%d", &num)
+	if err != nil {
+		log.Fatalf("failed to create server: %v", err)
+	}
+
+	node := &pb.Node{Port: num}
+	log.Printf("My ID is %s and my node is: %v", chordID, node)
+}
+
+func runGrpcServer(config util.Config) {
+
 	server, err := gapi.NewServer(config)
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
 	grpcServer := grpc.NewServer()
+
+	// call initNode fn
+	initNode(config, grpcServer)
+
 	pb.RegisterChordServer(grpcServer, server)
 	reflection.Register(grpcServer)
-	listener, err:= net.Listen("tcp", config.ServerAddress)
+	listener, err := net.Listen("tcp", config.ServerAddress)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
